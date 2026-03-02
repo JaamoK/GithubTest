dc192c450ec15d842753e8e20b6e46f71c337966
JaamoK <jarno.kankainen1@gmail.com>
2026-03-02 12:47:54 +0200

    Poistin commit tekstin riviltä 8

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,7 +5,7 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("VOI JEHNA KU ON SIISTI COMMIT");
+        Debug.Log("VOI JEHNA KU ON SIISTI");
     }
 
     // Update is called once per frame
