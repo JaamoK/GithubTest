a32653088aa4ca47f77e193de14c02ffb64b6c8e
JaamoK <jarno.kankainen1@gmail.com>
2026-03-02 12:48:31 +0200

    Voi pois rivi 8

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,7 +5,7 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("VOI JEHNA KU ON SIISTI COMMIT");
+        Debug.Log("JEHNA KU ON SIISTI COMMIT");
     }
 
     // Update is called once per frame
