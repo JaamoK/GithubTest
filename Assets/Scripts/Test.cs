1fe16194e14ee741534ca0211d089519debafe7f
JaamoK <jarno.kankainen1@gmail.com>
2026-03-02 12:47:54 +0200

    Poistin commit tekstin riviltä 8

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -6,6 +6,7 @@ public class Test : MonoBehaviour
     void Start()
     {
         Debug.Log("JEHNA KU ON SIISTI COMMIT");
+        Debug.Log("VOI JEHNA KU ON SIISTI");
     }
 
     // Update is called once per frame
